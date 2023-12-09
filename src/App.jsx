9e2d46d0f8743fc39eab322117b0f31234177a68
Donpere <doumemarion@hotmail.com>
2023-12-09 13:07:20 +0100

    add style.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 function App() {
-	const handleClick = () => {
-	alert("j'ai cliqué sur le titre")
-	}
+	const handleClick = (e, action) => {
+	console.log(e);
+	alert(`J'ai cliqué sur le bloc vert : ${action}`);
+	};
 	
 	return <>
-		<h1 onClick={handleClick} style={{textAlign: "center", fontFamily: "arial"}}>TITRE</h1>
+		<h1 style={{textAlign: "center", fontFamily: "arial"}}>TITRE</h1>
 
-		<div style={{height: "50vh",backgroundColor: "green", zIndex: "0"}}></div>
+		<div
+			className="green-blck"
+			onClick={(e) => handleClick(e, 'Effectuer une action spécifique')}
+		></div>
 		</>
 	}
 	
